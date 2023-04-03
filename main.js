4b8b41f074384e6f613444ca35ca972e257b169c
Alex-Arce <arcealejandrino@gmail.com>
2023-04-03 12:58:43 -0700

    added basic info to all three files

---
+++ main.js
@@ -0,0 +1,36 @@
+  /*----- constants -----*/
+const words = [
+  'TACOS GOBERNADOR',
+  'SOPE',
+  'AGUACHILE',
+  'CHILORIO',
+  'TOSTADAS',
+  'TAMALE DE ELOTE',
+  'FLAUTAS',
+  'CHILE RELLENO',
+  'MENUDO'
+  'BIRRIA'
+]
+const images = [
+  'url(img/tacos-gobernador.png)',
+  'url(img/SOPE.png)',
+  'url(img/aguachile.png)',
+  'url(img/CHILORIO.png)',
+  'url(img/TOSTADA.png)',
+  'url(img/TAMALE.png)',
+  'url(img/FLAUTAS.png)',
+  'url(img/chile-rellenos.png)',
+  'url(img/MENUDO.png)',
+  'url(img/birria.png)'
+]
+
+  /*----- state variables -----*/
+
+
+  /*----- cached elements  -----*/
+
+
+  /*----- event listeners -----*/
+
+
+  /*----- functions -----*/
